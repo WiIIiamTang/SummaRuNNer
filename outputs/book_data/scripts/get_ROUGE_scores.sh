dd4fe8b590c404329a8057a46f375da08ceef093
APorporino <anthony.porporino21@gmail.com>
2021-12-15 22:20:21 -0500

    Added rouge script to get scores

--- outputs/book_data/scripts/get_ROUGE_scores.sh
+++ outputs/book_data/scripts/get_ROUGE_scores.sh
@@ -1,32 +1,36 @@
-
+cd ../..
 echo "75"
 echo
 echo
-python ../../eval_config.py -b 75 -ref ../data/ref_75/ -hyp ../data/hyp_75/ >> ../scores/75.txt
+path_to_data_hyp="./book_data/data/sumaG/hyp"
+path_to_data_ref="./book_data/data/sumaG/ref"
+score_name="./book_data/scores/sumaG"
+
+python eval_config.py -b 75 -ref ${path_to_data_ref}_75/ -hyp ${path_to_data_hyp}_75/ >> ${score_name}_75.txt
 
 
 echo "275"
 echo
 echo
-python ../../eval_config.py -b 275 -ref ../data/ref_275/ -hyp ../data/hyp_275/ >> ../scores/275.txt
+python eval_config.py -b 275 -ref ${path_to_data_ref}_275/ -hyp ${path_to_data_hyp}_275/ >> ${score_name}_275.txt
 
 
 echo "Top 15"
 echo
 echo
-python ../../eval_config.py -ref ../data/ref_top_15/ -hyp ../data/hyp_top_15/ >> ../scores/top_15.txt
+python eval_config.py -ref ${path_to_data_ref}_top_15/ -hyp ${path_to_data_hyp}_top_15/ >> ${score_name}_top_15.txt
 
 
 
 echo "Top 20"
 echo
 echo
-python ../../eval_config.py -ref ../data/ref_top_20/ -hyp ../data/hyp_top_20/ >> ../scores/top_20.txt
+python eval_config.py -ref ${path_to_data_ref}_top_20/ -hyp ${path_to_data_hyp}_top_20/ >> ${score_name}_top_20.txt
 
 
 echo "Top 25"
 echo
 echo
-python ../../eval_config.py -ref ../data/ref_top_25/ -hyp ../data/hyp_top_25/ >> ../scores/top_25.txt
+python eval_config.py -ref ${path_to_data_ref}_top_25/ -hyp ${path_to_data_hyp}_top_25/ >> ${score_name}_top_25.txt
 
 
