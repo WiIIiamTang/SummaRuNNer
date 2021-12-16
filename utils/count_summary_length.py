076723b9af0ef1bed5e67d50735bb16aa0c68d41
APorporino <anthony.porporino21@gmail.com>
2021-12-16 01:12:10 -0500

    Fixed book script to show num words

--- utils/count_summary_length.py
+++ utils/count_summary_length.py
@@ -4,8 +4,8 @@ import json
 total_summary_length = 0
 total_document_length = 0
 
-total_summary_length_chars = 0
-total_document_length_chars = 0
+total_summary_length_words = 0
+total_document_length_words = 0
 
 total_books = 0
 with open("../data/test.json", "r") as fp:
@@ -13,12 +13,13 @@ with open("../data/test.json", "r") as fp:
         total_books += 1
         entry = json.loads(line)
         total_document_length += len(entry["doc"].split("\n"))
-        total_summary_length+= len(entry["summaries"].split("\n"))
+        total_summary_length += len(entry["summaries"].split("\n"))
 
-        total_document_length_chars += len(entry["doc"])
-        total_summary_length_chars += len(entry["summaries"])
+        total_document_length_words += len(entry["doc"].split(" "))
+        total_summary_length_words += len(entry["summaries"].split(" "))
 
-print("Average summary length: " + str(total_summary_length / total_books))
-print("Average doument length: " + str(total_document_length / total_books))
-print("Average summary length chars: " + str(total_summary_length_chars / total_books))
-print("Average doument length chars: " + str(total_document_length_chars / total_books))
+
+print("Average summary length sentences: " + str(total_summary_length / total_books))
+print("Average document length sentences: " + str(total_document_length / total_books))
+print("Average summary length words: " + str(total_summary_length_words / total_books))
+print("Average document length words: " + str(total_document_length_words / total_books))
